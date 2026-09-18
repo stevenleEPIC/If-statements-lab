2fec846cc776f42ec15d2ababb9b870de5a832a7
stevenleEPIC <leqsteven@yahoo.com>
2026-09-18 14:16:07 -0600

    Add files via upload

--- meal_time.py
+++ meal_time.py
@@ -1,18 +1,20 @@
 def main():
-    t = input("what time is it?: ")
-    t=convert(t)
-    print(t)
+    time = input("what time is it?: ")
+    time=convert(time)
+    if time >= 7 and time <= 8:
+        print("breakfast time")
+    elif time>= 12 and time<= 13:
+        print("lunch time")
+    elif time>=18 and time<=19:
+        print("dinner time")
 
 def convert(time):
     h, m = time.split(":")
     h = int(h)
     m = int(m)
-    t = h + m / 60
-    return t
+    time = h + m / 60
+    return time
 
 if __name__ == "__main__":
     main()
-    if time>= 7.0 and t<= 8.0:
-        print("breakfast time")
-    elif time>= 12.0 and t<= 13.0:
-        print("dinner time")
+
